6e18de8f4e907271129d4affa9af10a9e7361eb9
Vijith B <vijubalak@gmail.com>
2021-06-05 20:29:52 +0530

    Adding components

--- 1-hooks/src/App.js
+++ 1-hooks/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassCounter from './components/ClassCounter/ClassCounter';
+import HookCounter from './components/HookCounter/HookCounter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClassCounter />
+      <HookCounter />
     </div>
   );
 }
